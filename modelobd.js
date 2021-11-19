57a3ec8fbe8a80f59104de0c5aac0ce3e9104bdb
anfisbena <arcilacarmona@gmail.com>
2021-11-18 21:30:21 -0500

    modelo final

--- modelobd.js
+++ modelobd.js
@@ -1,23 +1,27 @@
 // NEW BEGINS
 //usuarios
+
 db.users.insertMany([
         {
                 email:"psoto@udea.edu.co",
                 contraseña:"ZCd3ljwpbVsi",
                 documentId:"1000",
-                documentType:"CID"
+                documentType:"CID",
                 nombre:"Pastora",
-                apellido:"Soto"
-                rol:"Admin",
-                status: true,
-                //codigoProyecto,rolProyecto,fechaIngreso,fechaEgreso
-                proyectos:[("ING0003","lider",ISODate(2021-10-15),ISODate("")),("ADM1020","miembro",ISODate(2021-10-15),ISODate(""))],
-        }
-])
+                apellido:"Soto",
+                rol:"lider", //Admin, estudiante, lider
+                estado: "autorizado", //autorizado,pendiente,no autorizado
+                //codigoProyecto,rolProyecto,fechaIngreso,fechaEgreso    Preguntar al profesor
+                //proyectos:[("ING0003",ISODate("2021-10-15"),ISODate("")),("ADM1020",ISODate(2021-10-15),ISODate(""))],
+                inscripciones:[
+                        {codigoProyecto:"ING0003",inscripcion:"Aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")},
+                        {codigoProyecto:"ACA47",inscripcion:"rechazada",fechaIngreso:ISODate(""),fechaEgreso:("")}
+                ]
+        }])
 
 db.proyectos.insertMany([
         {
-                CodigoProyecto:"ING0003",
+                codigoProyecto:"ING0003",
                 nombre:"Semillero de la vida eterna",
                 objetivos:{
                         generales:"Activar los ciclos de regeneracion celular a demanda",
@@ -25,16 +29,21 @@ db.proyectos.insertMany([
                                 "Secuencia genetica para la activacion de la regeneracion celular",
                                 "Generar un farmaco activador","Entregar los papers bajo licencia libre"
                         ]
-                }
+                },
                 Presupuesto:{
                         total:100000,
                         moneda:"COP",
-                        gastado:41500;
-                }
+                        gastado:41500,
+                },
                 fInicio:ISODate("2020-12-03"),
-                fFinal:ISODate(""),
-                estadoProyecto:true,
-                fase:"Iniciado"
+                fFinal:ISODate("2021-03-10"),
+                estadoProyecto:"activo", //activo inactivo
+                fase:"iniciado", //iniciado, en desarrollo, finalizado
+                avances:{
+                        fecha:ISODate(""),
+                        descripcion:"",
+                        LeadObserv:"",
+                }
         }
 ])
 
@@ -54,7 +63,7 @@ db.roles.insertMany([
                 gestionPerfil:true
         },
         {
-                rol:"miembro",
+                rol:"estudiante",
                 gestionUsuarios:false,
                 gestionProyecto:false,
                 gestionMiembros:false,
