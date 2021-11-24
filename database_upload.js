dd860efda94196603803bf7649dc80ef69614f77
anfisbena <republicacoc@gmail.com>
2021-11-24 11:45:15 -0500

    s

--- database_upload.js
+++ database_upload.js
@@ -266,5 +266,213 @@ db.users.insertMany([
     inscripciones:[
       {codigoProyecto:"ING0002",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
     ]
-  }
+  },
+  {
+    email:"user11@udea.edu.co",
+    contraseña:"CwaeGlYqRXONUrC",
+    documentId:"1011",
+    documentType:"CID",
+    nombre:"Nahia",
+    apellido:"Zapata",
+    rol:"lider", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0003",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user12@udea.edu.co",
+    contraseña:"47TCfETEBvlTbGW",
+    documentId:"1012",
+    documentType:"CID",
+    nombre:"George",
+    apellido:"Ochoa",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0003",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user13@udea.edu.co",
+    contraseña:"AgEvhIMo7ndV9qK",
+    documentId:"1013",
+    documentType:"CID",
+    nombre:"Gladys",
+    apellido:"Mellado",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0003",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user14@udea.edu.co",
+    contraseña:"gCwdVODN8ok2Fwp",
+    documentId:"1014",
+    documentType:"CID",
+    nombre:"Margarita",
+    apellido:"Pazos",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0003",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user15@udea.edu.co",
+    contraseña:"VglmAD6ly14J0XI",
+    documentId:"1015",
+    documentType:"CID",
+    nombre:"Ariana",
+    apellido:"Moya",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0003",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user16@udea.edu.co",
+    contraseña:"rAhtqvhpaGu9Sis",
+    documentId:"1016",
+    documentType:"CID",
+    nombre:"Ignacia",
+    apellido:"Tejera",
+    rol:"lider", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0004",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user17@udea.edu.co",
+    contraseña:"z1FjuA1bvSYWjGe",
+    documentId:"1017",
+    documentType:"CID",
+    nombre:"Hilario",
+    apellido:"Ramiro",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0004",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user18@udea.edu.co",
+    contraseña:"VUI85V20xkECYqy",
+    documentId:"1018",
+    documentType:"CID",
+    nombre:"Bienvenido",
+    apellido:"Rubio",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0004",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user19@udea.edu.co",
+    contraseña:"XuIXzwWHU9G4Vfq",
+    documentId:"1019",
+    documentType:"CID",
+    nombre:"Saida",
+    apellido:"Berrocal",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0004",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user20@udea.edu.co",
+    contraseña:"Um2jSwlTX91AnIi",
+    documentId:"1020",
+    documentType:"CID",
+    nombre:"Jordi",
+    apellido:"Lamas",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0004",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user21@udea.edu.co",
+    contraseña:"7pH2Bw7XV7zl2lQ",
+    documentId:"1021",
+    documentType:"CID",
+    nombre:"Teodoro",
+    apellido:"Padilla",
+    rol:"lider", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0005",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user22@udea.edu.co",
+    contraseña:"G8XlFz5Gb8IqWe3",
+    documentId:"1022",
+    documentType:"CID",
+    nombre:"Coral",
+    apellido:"Zaragoza",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0005",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user23@udea.edu.co",
+    contraseña:"pjjNAfmCiEiLRB3",
+    documentId:"1023",
+    documentType:"CID",
+    nombre:"Irati",
+    apellido:"Lago",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0005",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user24@udea.edu.co",
+    contraseña:"SveP0e41DiyjJNq",
+    documentId:"1024",
+    documentType:"CID",
+    nombre:"Susana",
+    apellido:"Hurtado",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0005",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"user25@udea.edu.co",
+    contraseña:"4H9V49TEfKhSUtE",
+    documentId:"1025",
+    documentType:"CID",
+    nombre:"Jacqueline",
+    apellido:"Mansilla",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0005",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
+  {
+    email:"userAdmin@udea.edu.co",
+    contraseña:"4H9V49TEfKhSUtE",
+    documentId:"1100",
+    documentType:"CID",
+    nombre:"Ona",
+    apellido:"Jimenez",
+    rol:"estudiante", //admin, estudiante, lider
+    estado: "autorizado", //autorizado,pendiente,no autorizado
+    inscripciones:[
+      {codigoProyecto:"ING0005",inscripcion:"aceptada",fechaIngreso:ISODate("2020-12-03"),fechaEgreso:("")}
+    ]
+  },
 ])
