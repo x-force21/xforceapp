0e5fdd23e0c365829f6e300e0259e8945993f482
LAPTOP-0UL5E3M3\ruben <rubenchod22@gmail.com>
2021-12-08 00:07:34 -0500

    GRAL se adiciona librería de cors para poder realizar las peticiones desde el front end

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { graphqlHTTP } = require('express-graphql')
 const { readFileSync } = require ('fs')
 const { join } = require ('path')
 const resolvers = require ('./lib/resolvers')
+const cors = require('cors');
 
 const app = express()
 const port = process.env.port || 3001
@@ -19,6 +20,8 @@ const typeDefs = readFileSync (
 const schema = makeExecutableSchema({
     typeDefs, resolvers
 })
+
+app.use(cors());
 
 app.use ('/api', graphqlHTTP ({
     schema: schema,
