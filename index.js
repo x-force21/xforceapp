741f47d4dca13c7121020de981d363931bcd7936
LAPTOP-0UL5E3M3\ruben <rubenchod22@gmail.com>
2021-12-17 10:38:19 -0500

    GRAL se modifica variable

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const resolvers = require ('./lib/resolvers')
 const cors = require('cors');
 
 const app = express()
-const PORT = process.env.port || 9000
+const PORT = process.env.PORT || 9000
 //const PORT = 3001 || 9000;
 const hostname = "0.0.0.0";
 
